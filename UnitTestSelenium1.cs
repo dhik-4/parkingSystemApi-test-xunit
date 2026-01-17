17da6fee5bbc07431614b544c0cc10c2c014a991
LAPTOP-2HP7GB51\Lenovo <andhikaekananda@gmail.com>
2026-01-17 22:08:24 +0700

    add test API in selenium test

--- UnitTestSelenium1.cs
+++ UnitTestSelenium1.cs
@@ -1,34 +1,62 @@
-﻿using OpenQA.Selenium;
-using Xunit;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System.Text.Json;
 
 namespace ParkingSystemAPI.Test.xUnit
 {
     public class UnitTestSelenium1 : IDisposable
     {
         private readonly IWebDriver driver;
+        private readonly string _baseUrl;
 
         public UnitTestSelenium1()
         {
+            var config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.Test.json", optional: false)
+            .Build();
+
+            // Read values
+            _baseUrl = config["CustomVariable:BaseUrlCall"];
+
             //this.driver = _driver;
             driver = new ChromeDriver();
             driver.Manage().Window.Maximize();
+
+            //_config = factory.Services.GetRequiredService<IConfiguration>();
+            //_baseUrl = _config["CustomVariable:BaseUrlCall"];
         }
 
 
         [Fact]
-        public void Open_Google_Should_Work()
+        public void Open_Google_Work()
         {
             driver.Navigate().GoToUrl("https://www.google.com");
 
             Assert.Contains("Google", driver.Title);
 
-            Thread.Sleep(5000);
+            Thread.Sleep(3000);
+        }
+
+        [Fact]
+        public void APIVehicleGetTrial()
+        {
+            driver.Navigate().GoToUrl(_baseUrl + "api/vehicle");
+
+            //var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            //wait.Until(d => d != null);
+
+            string pageSrc = driver.PageSource;
+
+            //Assert.Contains("\"ok\":false", driver.PageSource);
+
+
+            Thread.Sleep(3000);
+
+            
         }
 
         public void Dispose()
