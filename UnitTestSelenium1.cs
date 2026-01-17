ab5fd405a2267263a48ab294e2fd5bdfa9a3359a
LAPTOP-2HP7GB51\Lenovo <andhikaekananda@gmail.com>
2026-01-17 20:46:22 +0700

    add selenium test

---
+++ UnitTestSelenium1.cs
@@ -0,0 +1,39 @@
+﻿using OpenQA.Selenium;
+using Xunit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+
+namespace ParkingSystemAPI.Test.xUnit
+{
+    public class UnitTestSelenium1 : IDisposable
+    {
+        private readonly IWebDriver driver;
+
+        public UnitTestSelenium1()
+        {
+            //this.driver = _driver;
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+        }
+
+
+        [Fact]
+        public void Open_Google_Should_Work()
+        {
+            driver.Navigate().GoToUrl("https://www.google.com");
+
+            Assert.Contains("Google", driver.Title);
+
+            Thread.Sleep(5000);
+        }
+
+        public void Dispose()
+        {
+            driver.Quit();
+        }
+    }
+}
