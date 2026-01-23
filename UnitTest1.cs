ecab5b1dfc7ffba9d1ac6eb7009d5fc7d6f588f8
LAPTOP-2HP7GB51\Lenovo <andhikaekananda@gmail.com>
2026-01-23 19:53:40 +0700

    update missing file

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -5,6 +5,9 @@ using System.Net;
 using System.Text.Json;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using ParkingSystemAPI.CustomModels.Vehicle;
+using ParkingSystemAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ParkingSystemAPI.Test.xUnit
 {
@@ -13,12 +16,22 @@ namespace ParkingSystemAPI.Test.xUnit
         private readonly HttpClient _client;
         private readonly IConfiguration _config;
         private string _baseUrl;
+        private readonly AppDbContext _dbContext;
+        private readonly IServiceScopeFactory _scopeFactory;
 
         public UnitTest1(CustomWebApplicationFactory factory)
         {
             _client = factory.CreateClient();
             _config = factory.Services.GetRequiredService<IConfiguration>();
             _baseUrl = _config["CustomVariable:BaseUrlCall"];
+
+            _dbContext = factory.Services
+            .CreateScope()
+            .ServiceProvider
+            .GetRequiredService<AppDbContext>();
+
+            //_clientProgram = fact.CreateClient();
+            _scopeFactory = factory.Services.GetRequiredService<IServiceScopeFactory>();
         }
 
         [Fact]
@@ -31,6 +44,48 @@ namespace ParkingSystemAPI.Test.xUnit
             var product = await response.Content.ReadFromJsonAsync<dynamic>();
             JsonElement _jElem = product;
             Assert.True(_jElem.EnumerateArray().Count() >= 0);
+        }
+
+        [Fact]
+        public async Task Swagger_Should_Load()
+        {
+            var response = await _client.GetAsync("/swagger/index.html");
+            Assert.True(response.IsSuccessStatusCode);
+        }
+
+        [Fact]
+        public async Task AddVehicle_Test()
+        {
+            var data = new 
+            {
+                Code = "SB",
+                VehicleName = "Speed Boat Test"
+            };
+
+            var strategy = _dbContext.Database.CreateExecutionStrategy();
+
+            await strategy.ExecuteAsync(async () =>
+            {
+                using var transaction = await _dbContext.Database.BeginTransactionAsync();
+
+                // POST
+                var response = await _client.PostAsJsonAsync(
+                    "/api/vehicle",
+                    data
+                );
+
+                response.EnsureSuccessStatusCode();
+                Assert.True(response.IsSuccessStatusCode);
+
+                // ASSERT (inside transaction)
+                var vehicle = await _dbContext.VehicleMasters
+                    .FirstOrDefaultAsync(v => v.VehicleName == data.VehicleName);
+
+                Assert.NotNull(vehicle);
+
+                // ROLLBACK
+                await transaction.RollbackAsync();
+            });
         }
 
         [Fact]
